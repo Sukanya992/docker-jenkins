5f260100929ae57ac21050c8d36bb3f98d4b1372
Sukanya992 <sukanya09200206@gmail.com>
2025-03-06 12:27:49 +0530

    Update App.java

--- src/main/java/com/example/app/App.java
+++ src/main/java/com/example/app/App.java
@@ -1,7 +1,21 @@
 package com.example.app;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins!");
+        SpringApplication.run(App.class, args);
+    }
+
+    @GetMapping
+    public String hello() {
+        return "Hello, Jenkins!";
     }
 }
